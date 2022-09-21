41e921505244d2f28cfc0d66ace5c0d284e610ff
Alex Denysov <Alex.Denysov@in-syncgroup.com>
2022-09-21 09:38:12 +0100

    Add project files.

---
+++ personcentredsoftwareN/GeneralHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace personcentredsoftwareN
+{
+    public class GeneralHelper
+    {
+        public static int GetPrevIndex(int currentIndex, int listLength)
+        {
+            var prevIndex = currentIndex == 0 ? listLength - 1 : currentIndex - 1;
+            return prevIndex;
+        }
+
+        public static Dictionary<int, int> GetDictionaryFromArray(string[] stringArray)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                var indexofThisStation = i;
+                var indexOfPrevStaion = GetPrevIndex(i, stringArray.Length);
+
+                var thisStation = stringArray[indexofThisStation];
+                var prevStaion = stringArray[indexOfPrevStaion];
+
+                var thisStationSplit = thisStation.Split(":");
+                var prevStationSplit = prevStaion.Split(":");
+
+                var difrence = Int32.Parse(thisStationSplit[0]) - Int32.Parse(prevStationSplit[1]);
+
+                dict.Add(indexofThisStation, difrence);
+            }
+
+            return dict;
+        }
+
+        public static List<int> CheckCircularJorneyForIndex(int index, Dictionary<int, int> dict)
+        {
+            var listOfItemsThatCanPass=  new List<int>();
+            var prevIndex = GeneralHelper.GetPrevIndex(index, dict.Count);
+            var amountOfGas = dict[index];
+            
+            for (int i = 0; i < dict.Count; i++)
+            {
+                var val = dict[prevIndex];
+                amountOfGas += dict[prevIndex];
+                if (amountOfGas > 0)
+                {
+                    listOfItemsThatCanPass.Add(prevIndex);
+                }
+                prevIndex = GeneralHelper.GetPrevIndex(prevIndex, dict.Count);
+            }
+
+            return listOfItemsThatCanPass;
+        }
+    }
+}
---
+++ personcentredsoftwareN/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace personcentredsoftwareN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AS I am doing this excersise from memory I am asyming that in the string "4:2" 
+            //4 represennts amount og Gas on this station
+            //and 2 is represents amount of gas needed to get to this station from prevous station
+
+            string[] stringArray = {"4:0", "2:1", "3:2", "5:12", "3:1"};
+            int inputArrayLength = stringArray.Length;
+            // lets create a dictuanary where index fill be index of station in stringArray and second number will be represended by diffrence beween amount of gas on prev station and amount of gas needed to this station 
+            var dict = GeneralHelper.GetDictionaryFromArray(stringArray);
+
+            // finding potential station that we may not reach
+            var indexes = dict.Where(x => x.Value < 0).Select(x => x.Key).ToList();
+
+            var indexesOfStationsThatCircularJorneyCanFinish = new List<int>();
+            
+            //cheking each jorney where we may run out of gas
+            foreach (var index in indexes)
+            {
+                indexesOfStationsThatCircularJorneyCanFinish.AddRange(GeneralHelper.CheckCircularJorneyForIndex(index, dict));
+            }
+
+            var uniqueStaions = indexesOfStationsThatCircularJorneyCanFinish.Distinct();
+            Console.WriteLine("Indexes of the staion that you can start jorney to finish full circle " + String.Join(", ", uniqueStaions.ToArray()));
+        }
+
+      
+    }
+}
